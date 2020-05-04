721194b18245e1d9545e2fa7c48771e26db122f6
Daniel Kutik <daniel.kutik@lavawerk.com>
2020-05-04 14:05:23 +0800

    Fix NPE in InCallPresenter
    Handling case where call.getHandle() returns null in
    InCallPresenter#setDisconnectCauseForMissingAccounts
    
    Test: reran monkeyrunner
    Issue: 155582943
    Signed-off-by: Daniel Kutik <daniel.kutik@lavawerk.com>
    Change-Id: I2caaf9fc063a5673b369f196ceaf4ca589316bc7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Pranav Temkar <pranavtemkar@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- java/com/android/incallui/InCallPresenter.java
+++ java/com/android/incallui/InCallPresenter.java
@@ -1373,7 +1373,8 @@ public class InCallPresenter implements CallList.Listener, AudioModeProvider.Aud
                 PhoneAccountSuggestion.class);
 
     if (phoneAccountSuggestions == null || phoneAccountSuggestions.isEmpty()) {
-      String scheme = call.getHandle().getScheme();
+      Uri callHandle = call.getHandle();
+      String scheme = callHandle == null ? "null" : callHandle.getScheme();
       final String errorMsg =
           PhoneAccount.SCHEME_TEL.equals(scheme)
               ? context.getString(R.string.callFailed_simError)
